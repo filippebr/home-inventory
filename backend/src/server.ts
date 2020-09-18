9e07d03a5319893a14fe7c6984b429bb19f3c89d
Filippe Braga <filippeffx@hotmail.com>
2020-09-17 23:50:22 -0300

    Jest test fixed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import app from './app';
 const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  // console.log(`Listening at http://localhost:${port}`);
 });
 
 export default app;
