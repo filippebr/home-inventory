a02b65152281f33914b5c085db7e5bde71416ffb
Filippe Braga <filippeffx@hotmail.com>
2020-10-22 22:45:06 -0300

    Created a setup folder with supertest

--- backend/src/tests/routes.spec.ts
+++ backend/src/tests/routes.spec.ts
@@ -1,23 +1,23 @@
-import supertest from 'supertest';
-import http from 'http';
-import app from '../server';
+import { server } from './setup';
+// import http from 'http';
+// import app from '../server';
 
 describe('GET /', () => {
-  let server: http.Server;
-  let request: any;
+  // let server: http.Server;
+  // let request: any;
 
-  beforeAll((done) => {
-    server = http.createServer(app);
-    server.listen(done);
-    request = supertest.agent(server);
-  });
+  // beforeAll((done) => {
+  //   server = http.createServer(app);
+  //   server.listen(done);
+  //   request = server.agent(server);
+  // });
 
-  afterAll((done) => {
-    return server && server.close(done);
-  });
+  // afterAll((done) => {
+  //   return server && server.close(done);
+  // });
 
   it('should respond with a message', async () => {
-    const response = await request
+    const response = await server
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200);
