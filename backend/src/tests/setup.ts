cfd3781557e306eff78235511907b3246f9bd557
Filippe Braga <filippeffx@hotmail.com>
2020-10-28 21:45:52 -0300

    Installed some dev packages

--- backend/src/tests/setup.ts
+++ backend/src/tests/setup.ts
@@ -1,4 +1,8 @@
 import supertest from 'supertest';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
 import app from '../app';
 
+chai.use(sinonChai);
+export const { expect } = chai;
 export const server = supertest.agent(app);
