d52b5ab110719f521e91f5467455cc129b486aca
Filippe Braga <filippeffx@hotmail.com>
2020-11-05 23:07:40 -0300

    Inserted default routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import states from './api/states/states.routes';
+
 const router = express.Router();
+
+router.use('/states', states);
 
 router.get('/', (req, res) => {
   res.status(200).json({
